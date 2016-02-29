3a48b68914a829a17d4aa31f6d9bb9d7da92ac0a
Alastair <alastair.hawkes@gmail.com>
2016-02-29 12:16:47 +0000

    Initial commit

---
+++ src/com/sekwah/datashifter/DataShifter.java
@@ -0,0 +1,20 @@
+package com.sekwah.datashifter;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by on 29/02/2016.
+ *
+ * @author sekwah41
+ */
+public class DataShifter extends JavaPlugin {
+
+    public void onEnable() {
+
+    }
+
+    public void onDisable() {
+
+    }
+
+}
